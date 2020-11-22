d071e97248946cbff8f71484cf8b0be8107754d3
RAW <rwilliams01101@gmail.com>
2020-11-22 17:42:16 -0600

    updated connection notification

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected", socket.conn.id);
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
