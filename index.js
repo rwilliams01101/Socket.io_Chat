7b4d35e47e945e98d7bc7435d6b71d01de163d75
RAW <rwilliams01101@gmail.com>
2020-11-22 18:28:21 -0600

    created style.css

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var app = require("express")();
+var express = require("express");
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.use(express.static(__dirname + "/public"));
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.conn.id);
